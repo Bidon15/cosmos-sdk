e4935e3c4b00c336c5274312e6ce3d1bb04a2317
Nhu Viet Nguyen <braveryandglory@gmail.com>
2022-05-19 21:59:41 +0200

    oserr to osout

--- server/util.go
+++ server/util.go
@@ -150,9 +150,9 @@ func InterceptConfigsPreRunHandler(cmd *cobra.Command, customAppConfigTemplate s
 
 	var logWriter io.Writer
 	if strings.ToLower(serverCtx.Viper.GetString(flags.FlagLogFormat)) == tmcfg.LogFormatPlain {
-		logWriter = zerolog.ConsoleWriter{Out: os.Stderr}
+		logWriter = zerolog.ConsoleWriter{Out: os.Stdout}
 	} else {
-		logWriter = os.Stderr
+		logWriter = os.Stdout
 	}
 
 	logLvlStr := serverCtx.Viper.GetString(flags.FlagLogLevel)
